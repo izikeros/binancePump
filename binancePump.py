b1bc924948e93bd32de7dd3fa396ee1f9affc406
Krystian Safjan <ksafjan@gmail.com>
2022-03-29 08:38:31 +0200

    Disabled telegram bot by default

--- binancePump.py
+++ binancePump.py
@@ -274,7 +274,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--use-telegram-bot",
         action="store_true",
-        default=False,
+        default=True,
         help="enable telegram bot",
     )
     args = parser.parse_args()
