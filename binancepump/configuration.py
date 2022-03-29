334bfc3bf9a375a8f5d9b5e8243826f66a036b73
Krystian Safjan <ksafjan@gmail.com>
2022-03-29 12:32:25 +0200

    Update configuration.py

--- binancepump/configuration.py
+++ binancepump/configuration.py
@@ -1,4 +1,4 @@
-show_only_pair = "USDT"  # Select nothing for all, only selected currency will be shown
-show_limit = 1  # minimum top query limit
-min_perc = 0.05  # min percentage change
-last_symbol = "X"
+show_only_pair = "USDT"     # Select nothing for all, only selected currency will be shown
+show_limit = 1              # minimum top query limit
+min_perc = 0            # min percentage change (default 0.05)
+#last_symbol = "X"           # candle closing data
