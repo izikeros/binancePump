a3b2e42bd4f12bffc54f07c121c319314ca7ee54
Krystian Safjan <ksafjan@gmail.com>
2022-03-29 10:17:07 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="binancepump",
+    packages=["binancepump"],
+    version="0.1.0",
+    description="Detects pump and dump events on Binance exchange.",
+    author="https://github.com/ogu83, Krystian Safjan",
+    author_email="oguzkoroglu@gmail.com, ksafjan@gmail.com",
+    license="",
+    url="",
+)
